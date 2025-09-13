c3d42f5af1b267a286c2e8ec56b8a571892ffc96
Prathu <123509026+SeaM05@users.noreply.github.com>
2025-09-13 19:10:39 +0100

    reactor II

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const Hero = ({ scrollToSection }) => {
+const Hero = () => {
     const [formMessage, setFormMessage] = useState('');
     const [formData, setFormData] = useState({
         name: '',
